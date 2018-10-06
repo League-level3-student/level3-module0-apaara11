e3a9b5432b99863008d98ced8d3d6d4d6de8f4dc
apaara11 <league@WTS21.attlocal.net>
2018-10-06 15:55:04 -0700

    FindTheHiddenButton (IP), RobotRace (IP)

--- src/_00_IntroToArrays/_02_FindTheHiddenButton.java
+++ src/_00_IntroToArrays/_02_FindTheHiddenButton.java
@@ -16,8 +16,9 @@ public class _02_FindTheHiddenButton implements ActionListener{
 	JPanel panel;
 	
 	//1. create an array of JButtons. Don't initialize it yet.
-	
+	JButton[] jbutton;
 	//2 create an integer variable called hiddenButton
+	int hiddenButton=0;
 	
 	public static void main(String[] args) {
 		new _02_FindTheHiddenButton().start();
@@ -29,22 +30,31 @@ public class _02_FindTheHiddenButton implements ActionListener{
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		//3.  Ask the user to enter a positive number and convert it to an int
-		
+		String positiveNumber = JOptionPane.showInputDialog("Enter a positive number above 10 but below 673.");
+		int answer = Integer.parseInt(positiveNumber);
 		//4. Initialize the array of JButtons to be the size of the int created in step 3
+	
+			jbutton= new JButton[answer];
+		
 		
 		//5. Make a for loop to iterate through the JButton array
+		for(int x= 0; x< jbutton.length; x++) {
+			jbutton [x]= new JButton();
+			jbutton[x].addActionListener(this);
+			panel.add(jbutton[x]);
+		}
 			//6. initialize each JButton in the array
 			//7. add the ActionListener to each JButton
 			//8. add each JButton to the panel
 		
 		//9 add the panel to the window
-		
+		window.add(panel);
 		//10. call setExtendedState(JFrame.MAXIMIZED_BOTH) on your JFrame object.
-		
+		window.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		//11. set the JFrame to visible.
-		
+		window.setVisible(true);
 		//12. Give the user the instructions for the game.
-		
+		JOptionPane.showMessageDialog(null, "Find the ß©∑∂¥øπåœ hidden button..... it's in the name");
 		//13. initialize the hiddenButton variable to a random number less than the int created in step 3
 		
 		//14. Set the text of the JButton located at hiddenButton to read "ME"
@@ -60,7 +70,7 @@ public class _02_FindTheHiddenButton implements ActionListener{
 		JButton buttonClicked = (JButton)e.getSource();
 		
 		//17. if the hiddenButton is clicked, tell the user that they win.
-
+		
 		//18. else tell them to try again
 	}
 }
