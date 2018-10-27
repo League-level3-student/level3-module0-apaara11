b51cfaa60e186eb0cfc00e295712c8c8b81baba3
apaara11 <league@WTS21.attlocal.net>
2018-10-27 15:55:28 -0700

    Array Methods (IP) 2/3: Finish next week
    i need more common sense….

--- src/_01_Simple_Array_Algorithms/_00_1D_Array_Methods.java
+++ src/_01_Simple_Array_Algorithms/_00_1D_Array_Methods.java
@@ -1,26 +1,35 @@
 package _01_Simple_Array_Algorithms;
 
 public class _00_1D_Array_Methods {
+	
 	//1. Complete the method so that it returns the sum of all
 	//   of the integers in the array being passed in
 	public static int sumIntArray(int[] values) {
-
-		return 0;
+		int sum=0;
+	for(int i=0; i<values.length; i++ ) {
+		sum = sum + values[i];   
+		
+	}
+		return sum;
 	}
 	
 	//2. Complete the method so that it returns the average of all
 	//   of the integers in the array being passed in
 	public static double averageIntArray(int[] values) {
-				
-		return 0;
+		double average=0;
+	for(int i=0; i<values.length; i++ ) {
+			average = average + values[i];   
+		
+	}		
+		return average/values.length;
 	}
-	
 	
 	//3. Complete the method so that it returns true if the integer
 	//   array contains the value specified by the second parameter.
 	//   It should otherwise return false.
 	public static boolean containsIntValue(int[] array, int value) {
-	
+		
+			
 		return false;
 	}
 	
